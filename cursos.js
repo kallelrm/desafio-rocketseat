7ec0153516d873a9517a31e22c007c6e48c69151
kallelrm <kallelr@outlook.com>
2020-07-07 10:32:26 -0300

    Concluindo desafio / adiciona exportação e importação no js e no njk

---
+++ cursos.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    id: 'starter',
+    logo: 'https://app.rocketseat.com.br/static/media/Starter.64c237cc.svg',
+    title: 'Starter',
+    price: 'Gratuito',
+    modules: '6 módulos'
+  },
+  {
+    id: 'launchbase',
+    logo: 'https://app.rocketseat.com.br//static/media/LaunchBase.08285320.svg',
+    title: 'Launchbase',
+    price: 'Pago',
+    modules: '11 módulos'
+  },
+  {
+    id: 'gostack',
+    logo: 'https://skylab.rocketseat.com.br/static/media/GoStack.83a178a0.svg',
+    title: 'GoStack',
+    price: 'Pago',
+    modules: '15 módulos'
+  },
+];
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
+const courses = require('./cursos');
 
 const server = express();
 
@@ -9,14 +10,30 @@ server.set('view engine', 'njk');
 
 nunjucks.configure('views', {
   express: server,
+  autoescape: false,
 })
 
 server.get('/', function (req, res){
-  res.render('sobre');
+  const about = {
+    logo: 'logo.png',
+    name: 'COMPANHIA',
+    description: 'Aqui para facilitar sua vida',
+    techs: [
+      'Node.js',
+      'React',
+      'React Native',
+    ],
+    links: [
+      { name: 'Github', url: 'https://github.com/'},
+      { name: 'Instagram', url: 'https://instagram.com/'},
+      { name: 'Facebook', url: 'https://facebook.com/'},
+    ]
+  }
+  res.render('sobre', { about });
 });
 
 server.get('/cursos', function(req, res){
-  res.render('cursos');
+  res.render('cursos', { courses });
 });
 
 server.use(function(req, res) {
